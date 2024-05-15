ff354ff1eda1954ab8d0aaba14b9e8fe476f3b35
carolkeerthana <keerthana0497@gmail.com>
2024-05-15 19:07:29 +0530

    first commit

---
+++ src/components/LoginPage.js
@@ -0,0 +1,23 @@
+function LoginPage(){
+    return(
+        <html>
+            <body>
+                <div>
+                    <div>
+                        <h1>UTube</h1>
+                        <div>
+                        <h2>Sign in</h2>
+                        <span>to continue to UTube</span>
+                        </div>
+                    </div>
+                    <form>
+                        <input placeholder="Email"/>
+                        <input placeholder="Password"/>                       
+                    </form>
+                </div>
+            </body>
+        </html>
+    )
+}
+
+export default LoginPage;
---
+++ src/components/RegisterPage.js
@@ -0,0 +1,25 @@
+function RegisterPage(){
+    return(
+        <html>
+            <body>
+                <div>
+                    <div>
+                        <h1>UTube</h1>
+                        <div>
+                        <h2>Create a UTube Account</h2>
+                        </div>
+                    </div>
+                    <form>
+                        <input placeholder="Email"/>
+                        <input placeholder="Channel Name"/>
+                        <input placeholder="Password"/>
+                        <input placeholder="Confirm Password"/>    
+                        <button>SIGN UP</button>                   
+                    </form>
+                </div>
+            </body>
+        </html>
+    )
+}
+
+export default RegisterPage;
