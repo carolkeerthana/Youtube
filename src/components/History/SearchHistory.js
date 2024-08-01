1598b84e44d40f8fc31d866aab95b7ab8aa04fcc
carolkeerthana <keerthana0497@gmail.com>
2024-08-01 19:03:04 +0530

    update 35

--- src/components/History/SearchHistory.js
+++ src/components/History/SearchHistory.js
@@ -1,82 +1,88 @@
-import { fetchHistories } from './HistoryApi/GetHistoryApi';
-import './History.css'
-import React, { useEffect, useState } from 'react'
-import moment from 'moment';
-import closeIcon from '../../assets/close.png';
-import { deleteHistory } from './HistoryApi/DeleteHistoryApi';
+import { fetchHistories } from "./HistoryApi/GetHistoryApi";
+import "./History.css";
+import "./SearchHistory.css";
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+import closeIcon from "../../assets/close.png";
+import { deleteHistory } from "./HistoryApi/DeleteHistoryApi";
 
 const SearchHistory = ({ history, setHistory }) => {
-    // const [histories, setHistories] = useState([]);
-    const [error, setError] = useState('');
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
-    const [notification, setNotification] = useState('');
+  // const [histories, setHistories] = useState([]);
+  const [error, setError] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [notification, setNotification] = useState("");
 
-    const fetchHistoryVideos = async(page) => {
-        try {
-            const response = await fetchHistories(page, 'search');
-            console.log('API response:', response);
-            if(response.success && Array.isArray(response.data)){
-                setHistory(response.data);
-                setTotalPages(response.totalPages);
-            }  
-          } catch (error) {
-            setError('Error fetching data');
-            setHistory([]);
-          }
-    } 
-    useEffect(() => {
-        fetchHistoryVideos(currentPage);         
-}, [currentPage]);
-
-    // const handlePageChange = (page) => {
-    //     setCurrentPage(page)
-    // }
-
-    const handleDeleteHistory = async(historyId) => {
-        try {
-            const response = await deleteHistory(historyId);
-            if(response.success){
-                setHistory((prevHistories) => prevHistories.filter((history) => history._id !== historyId));
-                setNotification('History deleted successfully');
-                setTimeout(() => setNotification(''), 3000); // Hide notification after 3 seconds
-            }
-        } catch (error) {
-            console.error('Error deleting history:', error);
-        }
+  const fetchHistoryVideos = async (page) => {
+    try {
+      const response = await fetchHistories(page, "search");
+      console.log("API response:", response);
+      if (response.success && Array.isArray(response.data)) {
+        setHistory(response.data);
+        setTotalPages(response.totalPages);
+      }
+    } catch (error) {
+      setError("Error fetching data");
+      setHistory([]);
     }
+  };
+  useEffect(() => {
+    fetchHistoryVideos(currentPage);
+  }, [currentPage]);
+
+  // const handlePageChange = (page) => {
+  //     setCurrentPage(page)
+  // }
+
+  const handleDeleteHistory = async (historyId) => {
+    try {
+      const response = await deleteHistory(historyId);
+      if (response.success) {
+        setHistory((prevHistories) =>
+          prevHistories.filter((history) => history._id !== historyId)
+        );
+        setNotification("History deleted successfully");
+        setTimeout(() => setNotification(""), 3000); // Hide notification after 3 seconds
+      }
+    } catch (error) {
+      console.error("Error deleting history:", error);
+    }
+  };
 
   return (
-        <div className='left-side-history'>
-        <h2 className='search-h2'>Search History</h2>
-        {error ? (
-                <p>{error}</p>
-            ) : ( 
-                history && history.length > 0 ? (
-                    <>
-                   {history.map((history) => (
-                    <div className='search-history' key={history._id}>   
-                        <div className='search-text'>        
-                            <p>{history.searchText}</p>
-                            <button className='delete-button' onClick={()=>handleDeleteHistory(history._id)}><img src={closeIcon} alt=''/></button>
-                        </div>
-                        <div className='search-subtext'>
-                            <p className='search'>{moment(history.createdAt).fromNow()}</p>
-                        </div>
-                    </div>
-                    ))}
-                  {/* <Pagination
+    <div className="search-left-side-history">
+      <h2 className="search-h2">Search History</h2>
+      {error ? (
+        <p>{error}</p>
+      ) : history && history.length > 0 ? (
+        <>
+          {history.map((history) => (
+            <div className="search-history" key={history._id}>
+              <div className="search-text">
+                <p>{history.searchText}</p>
+                <button
+                  className="delete-button"
+                  onClick={() => handleDeleteHistory(history._id)}
+                >
+                  <img src={closeIcon} alt="" />
+                </button>
+              </div>
+              <div className="search-subtext">
+                <p className="search">{moment(history.createdAt).fromNow()}</p>
+              </div>
+            </div>
+          ))}
+          {/* <Pagination
                   currentPage = {currentPage}
                   totalPages = {totalPages}
                   onPageChange={handlePageChange}/>     */}
-                  </>          
-            ) : (
-                <p>No search history yet.</p>
-            )
-        )}
-        {notification && <div className='notification'>{notification}</div>}
-        </div>
-  )
-}
+        </>
+      ) : (
+        <p>No search history yet.</p>
+      )}
+      {notification && <div className="notification">{notification}</div>}
+    </div>
+  );
+};
 
-export default SearchHistory
+export default SearchHistory;
--- src/components/Subscriptions/Subscriptions.js
+++ src/components/Subscriptions/Subscriptions.js
@@ -1,58 +1,69 @@
-import React, { useEffect, useState } from 'react'
-import './Subscriptions.css'
-import { fetchSubscriptions } from './SubscriptionApi';
-import moment from 'moment';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import "./Subscriptions.css";
+import { fetchSubscriptions } from "./SubscriptionApi";
+import moment from "moment";
+import { Link } from "react-router-dom";
 
 const Subscriptions = () => {
-    const [subscriptions, setSubscriptions] = useState([]);
-    const [error, setError] = useState('');
+  const [subscriptions, setSubscriptions] = useState([]);
+  const [error, setError] = useState("");
 
-    const fetchSubscriptionVideos = async() => {
-        try {
-            const response = await fetchSubscriptions();
-            console.log('API response:', response);
-            if(response.success && Array.isArray(response.data)){
-                setSubscriptions(response.data);
-            }else{
-              setError(response.error);
-              setSubscriptions([]);
-            }   
-          } catch (error) {
-            setError('Error fetching data');
-            setSubscriptions([]);
-          }
-    } 
-    useEffect(() => {
-        fetchSubscriptionVideos();         
-}, [])
+  const fetchSubscriptionVideos = async () => {
+    try {
+      const response = await fetchSubscriptions();
+      console.log("API response:", response);
+      if (response.success && Array.isArray(response.data)) {
+        setSubscriptions(response.data);
+      } else {
+        setError(response.error);
+        setSubscriptions([]);
+      }
+    } catch (error) {
+      setError("Error fetching data");
+      setSubscriptions([]);
+    }
+  };
+  useEffect(() => {
+    fetchSubscriptionVideos();
+  }, []);
   return (
-      <div className='subscriptions'>
-        {error ? (
-                <p>{error}</p>
-            ) : ( 
-                subscriptions && subscriptions.length > 0 ? (
-                subscriptions.map((subscription) => {
-                    return(
-                    <Link to={`/watch/${subscription._id}`} className='subscriptions-card' key={subscription._id}>           
-                        <img src={`https://apps.rubaktechie.me/uploads/thumbnails/${subscription.thumbnailUrl}`} alt={subscription.title}/>
-                        <div className='subscriptions-details'>
-                        <img src={`https://apps.rubaktechie.me/uploads/avatars/${subscription.userId.photoUrl}`} alt={subscription.userId.channelName}/>
-                        <div>
-                        <h2>{subscription.title}</h2>
-                        <h3>{subscription.userId.channelName}</h3>
-                        <p>{subscription.views} views &bull; {moment(subscription.createdAt).fromNow()}</p>
-                        </div>
-                        </div>
-                    </Link>
-                    )
-                  })                 
-            ) : (
-                <p>No results found.</p>
-            )
-        )}
+    <div className="subscriptions">
+      {error ? (
+        <p>{error}</p>
+      ) : subscriptions && subscriptions.length > 0 ? (
+        subscriptions.map((subscription) => {
+          return (
+            <Link
+              to={`/watch/${subscription._id}`}
+              className="subscriptions-card"
+              key={subscription._id}
+            >
+              <img
+                src={`https://apps.rubaktechie.me/uploads/thumbnails/${subscription.thumbnailUrl}`}
+                alt={subscription.title}
+              />
+              <div className="subscriptions-details">
+                <img
+                  src={`https://apps.rubaktechie.me/uploads/avatars/${subscription.userId.photoUrl}`}
+                  alt={subscription.userId.channelName}
+                />
+                <div>
+                  <h2>{subscription.title}</h2>
+                  <h3>{subscription.userId.channelName}</h3>
+                  <p>
+                    {subscription.views} views &bull;{" "}
+                    {moment(subscription.createdAt).fromNow()}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          );
+        })
+      ) : (
+        <p>No results found.</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Subscriptions
+export default Subscriptions;
